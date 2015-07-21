bf21e4c038be20096c5f4e58e9438fbb771b818e
Michael F. Ward <mfwth@live.com>
2015-07-21 11:35:10 -0400

    boom

--- sched.rb
+++ sched.rb
@@ -5,14 +5,14 @@ require 'date'
 $DAYS = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"]
 
 
-#the following will load the current schedule.
-class Loader < Shoes
+#the following will load the current week / schedule.
+class Loader
   def initialize
     @date = DateTime.now #get current date
-    if @date.cwday === 7 
+    if @date.cwday === 7 #if its sunday, we're fresh
       @fresh = true
     else
-      @fresh = true
+      @fresh = false
     end
   end
   def weekday
@@ -29,26 +29,37 @@ class Loader < Shoes
         btns.push("#{$DAYS[x]} #{tmp}")
       end
     else
-      puts "hi!"
+      sundate = @date.day - @date.cwday
+      0.upto(6) do |x|
+        tmp = x+sundate
+        btns.push("#{$DAYS[x]} #{tmp}")
+      end
     end
     btns
   end
   def fresh?
     @fresh
   end
+  #for now, the employees will just be a .txt file, serparated by columns
+  def load_employees
+    path = "employees.txt"
+    fd = File.open(path, "r")
+    fd.each_line do |line|
+      #boobage for now
+    end
+  end
 end
 
 
 
 
-#        button("#{x}").style width:100, height:100
 
 
 
 Shoes.app(title: "Baesler's Scheduling Application") do
   def init
-    loader = Loader.new
     flow {
+      loader = Loader.new
       a = loader.spawn_buttons
       a.each {|x| button("#{x}").style width:100, height:100}
     }
