8f5ec2d234e29c0b92e4a725429db742cd65b352
Michael F. Ward <mfwth@live.com>
2015-07-22 12:44:15 -0400

    redid a bit of this and that to make the code cleaner

--- app.rb
+++ app.rb
@@ -1,23 +1,30 @@
 $LOAD_PATH << "."
-require 'lib/calendar'
-#require 'employee'
-#require 'dbengine'
-#require 'schedule'
-#require 'foursh'
-#require 'schutil'
+require 'lib/scheduler'
 #`update.bat` updater but dont run right now
 #defining constants
 
 Shoes.app(title: "Baesler's Scheduling Application", width: 1024, height: 768, resizable: true) do
-
+  banner "Baesler's Scheduling Application - Under Heavy Development"
   #USER INTERFACE -> FOURSH.RB#
   flow {
-    @shed = button "New"
-    @editbutton = button "Edit"
-    @employees = button "Employees"
-    @sdays = button "Special Days"
-    @makenote = button "Make Note"
-    @backbutton = button "Go Back"
+    @shed = button("New").click{
+      alert('hi!')
+    }
+    @editbutton = button("Edit").click {
+
+    }
+    @employees = button("Employees").click {
+
+    }
+    @sdays = button("Special Days").click {
+
+    }
+    @makenote = button("Make Note").click {
+
+    }
+    @backbutton = button("Go Back").click {
+
+    }
   }
   #END UI#
   #BEGIN SETTING PROPERTIES OF THE TOOLBAR BUTTONS
@@ -31,7 +38,7 @@ Shoes.app(title: "Baesler's Scheduling Application", width: 1024, height: 768, r
     @calbtns = []
     calendar.spawn_buttons.each do |btn|
       #btn.click {Schedule.load_schedule} #needs class obviously!
-      @calbtns.push(btn)
+      @calbtns.push(button(btn, width:75, height:75))
     end
   #END CALENDAR#
 
